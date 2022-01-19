1e6ebd91df9f1cba54f7e51a9b247855507d96c2
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-19 16:56:46 +0100

    working on project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+
+let url =
+  'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+let cb = '&callback=JSON_CALLBACK';
+
+// 'https://en.wikipedia.org/wiki/Special:Random'
+
 function App() {
+  const [wiki, setWiki] = useState('');
+  const [articles, setArticles] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(wiki);
+  };
+
   return (
     <main>
-      <h1>hellow</h1>
+      <header>
+        <h1>Free Code Camp</h1>
+        <h1>Wikipedia Viewer</h1>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={wiki}
+          onChange={(e) => setWiki(e.target.value)}
+        />
+      </form>
     </main>
   );
 }
